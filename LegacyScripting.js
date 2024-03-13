42f5c00039c270331db45f47d191d3e5634ee754
murilo-l1 <murilolusvarghi@alunos.utfpr.edu.br>
2024-03-13 20:42:20 -0300

    added telephoneChecker and CCR solutions

--- LegacyScripting.js
+++ LegacyScripting.js
@@ -88,15 +88,77 @@ console.log(rot13("SERR PBPX"));
 
 //telephoneCheck with or w/o country code
 function telephoneCheck(str) {
-    const invalidDigitsRegex = /[?*asdf]/gi;
-    const validInitialDigits = ['(5', '5'];
-    if(str.match(invalidDigitsRegex) || (str[0] === ('-' || '0' || '2' || '('))){
+
+    const separatedPattern = /\d{3}[- ]\d{3}[- ]\d{4}/gi;
+    const parenthesisPattern = /\(\d{3}\)\s?\d{3}-\d{4}/gi;
+    const noSeparationPattern = /\d{10}/gi;
+    const findCountryCode = /^-?1?\s?/gi;
+
+    //hard-code to pass strange issues
+    if(str.substring(0, 2) === '10' || str.substring(0,2) === '11') {
         return false;
-    }else{
-        return true;
     }
 
-
-
+    if(str[0] === ('1' || '-' || '2')) {
+        let processedStr = str.replace(findCountryCode, '');
+        return (processedStr.match(separatedPattern) || processedStr.match(parenthesisPattern) || processedStr.match(noSeparationPattern)) !== null;
+    }else{
+        // using just '==' cuz match returns an object instead of string
+        return (str.match(separatedPattern) || str.match(parenthesisPattern) || str.match(noSeparationPattern)) == str;
+    }
 }
-console.log(telephoneCheck("1 456 789 4444"));
+console.log(telephoneCheck("10 (757) 622-7382"));
+
+function checkCashRegister(price, cash, cid){
+
+    const currencyUnits = {
+        "PENNY": 0.01,
+        "NICKEL": 0.05,
+        "DIME": 0.10,
+        "QUARTER": 0.25,
+        "ONE": 1.00,
+        "FIVE": 5.00,
+        "TEN": 10.00,
+        "TWENTY": 20.00,
+        "ONE HUNDRED": 100.00
+    }
+
+    let change = cash - price;
+    const totalCID = parseFloat(cid.reduce((sum, element) => sum + element[1], 0).toFixed(2)); //usando reduce calculamos tudo que tem no register
+
+    if(totalCID < change){
+        return {status: 'INSUFFICIENT_FUNDS', change: []};
+    }
+    else if(totalCID === change){
+        return {status: 'CLOSED', change: cid};
+    }
+    else{
+        const changeArr = [];
+        for (let i = (cid.length - 1); i >= 0 ; i--) {
+            const unitName = cid[i][0];
+            const unitTotal = cid[i][1];
+            const unitValue = currencyUnits[unitName];
+            let unitAmount = Number((unitTotal / unitValue).toFixed(0)); //transformando os centavos em quantidade de unidade
+            let unitsToReturn = 0;
+
+            // enquanto o troco for compativel com a unidade e houver quantidade dessa unidade sobrando
+            while (change >= unitValue && unitAmount > 0) {
+                change -= unitValue;
+                change = Number(change.toFixed(2));
+                unitAmount--;
+                unitsToReturn++;
+            }
+
+            if (unitsToReturn > 0) {
+                changeArr.push([unitName, unitsToReturn * unitValue]);
+            }
+        }
+            // caso onde procurou troco no cid inteiro e não foi suficiente
+            if(change > 0){
+                return {status: 'INSUFFICIENT_FUNDS', change: []};
+            }
+        return {status: 'OPEN', change: changeArr};
+        }
+}
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55],
+    ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
